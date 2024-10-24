d2cbbc6b0c018321c1a387faafb43e0417e569c8
Francisco Antonio Aponte Rivas <faar2002@gmail.com>
2024-10-24 15:56:35 -0300

    Add files via upload

---
+++ busca/src/main/java/es/faponte/school/springboot/busca/InfoBusca.java
@@ -0,0 +1,20 @@
+package es.faponte.school.springboot.busca;
+
+public class InfoBusca {
+	private long idAsistente;
+	private long idReunion;
+	public long getIdAsistente() {
+		return idAsistente;
+	}
+	public void setIdAsistente(long idAsistente) {
+		this.idAsistente = idAsistente;
+	}
+	public long getIdReunion() {
+		return idReunion;
+	}
+	public void setIdReunion(long idReunion) {
+		this.idReunion = idReunion;
+	}
+	
+	
+}
